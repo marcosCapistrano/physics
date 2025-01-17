a2feb600b3237708c7c20c179b7a0aafb3e79f3b
Marcos Capistrano <mcapistrano50@gmail.com>
2025-01-16 21:06:32 -0300

    particle movement

---
+++ constants.go
@@ -0,0 +1,4 @@
+package main
+
+const FPS uint32 = 60
+const MILLISECS_PER_FRAME uint32 = 1000 / FPS
--- physics/particle.go
+++ physics/particle.go
@@ -2,17 +2,17 @@ package physics
 
 type Particle struct {
 	Position     Vec2
-	velocity     Vec2
-	acceleration Vec2
+	Velocity     Vec2
+	Acceleration Vec2
 
-	mass float32
+	Mass float32
 }
 
 func NewParticle(x, y, mass float32) Particle {
 	return Particle{
 		Position:     NewVec2(x, y),
-		velocity:     NewVec2(0, 0),
-		acceleration: NewVec2(0, 0),
-		mass:         mass,
+		Velocity:     NewVec2(0, 0),
+		Acceleration: NewVec2(0, 0),
+		Mass:         mass,
 	}
 }
